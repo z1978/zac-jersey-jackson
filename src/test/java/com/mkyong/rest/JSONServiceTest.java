89bfaaa9938f9c2e36f5e9eb5b857a2b2fb6451d
zac <zgithub@hotmail.com>
2018-02-01 20:54:43 +0900

    add test

---
+++ src/test/java/com/mkyong/rest/JSONServiceTest.java
@@ -0,0 +1,79 @@
+package com.mkyong.rest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class JSONServiceTest {
+
+	@Test
+	public void testGetTrackInJSON() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCreateTrackInJSON() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetClass() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testHashCode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEquals() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testClone() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNotify() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNotifyAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWaitLong() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWaitLongInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWait() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFinalize() {
+		fail("Not yet implemented");
+	}
+
+}
